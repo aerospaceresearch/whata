aa4315c4498ef9973e2c3ded7070c22af606eae7
Georg Grab <g.j.grab@gmail.com>
2017-04-30 11:17:59 +0200

    Initial Frontend commit: Vue.js Webpack scaffold; Leaflet; vue-material

---
+++ ui/src/components/MainPage.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="main-page">
+    <div class="phone-viewport">
+      <md-toolbar>
+        <md-button class="md-icon-button" @click.native="toggleLeftSidenav">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <span class="whata-logo whata-logo-small"></span>
+        <h2 class="md-title">Whata <span class="md-subheading">save the planet - classify water</span></h2>
+  
+        <span style="flex: 1;"></span>
+        <md-button class="md-icon-button">
+          <md-icon>search</md-icon>
+        </md-button>
+  
+        <md-button class="md-icon-button">
+          <md-icon>filter_list</md-icon>
+        </md-button>
+      </md-toolbar>
+  
+      <div>
+        <WaterMap></WaterMap>
+      </div>
+  
+      <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+        <md-toolbar class="md-large">
+          <div class="md-toolbar-container">
+            <h3 class="md-title">Sidenav content</h3>
+          </div>
+        </md-toolbar>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi cupiditate esse necessitatibus beatae nobis, deserunt ut est fugit, tempora deleniti, eligendi commodi doloribus. Nemo, assumenda possimus, impedit inventore perferendis iusto!</p>
+      </md-sidenav>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import WaterMap from '@/components/WaterMap.vue'
+
+export default {
+  name: 'MainPage',
+  components: { Header, WaterMap },
+
+  data() {
+    return {
+    }
+  },
+
+  methods: {
+    toggleLeftSidenav() {
+      this.$refs.leftSidenav.toggle();
+    },
+    toggleRightSidenav() {
+      this.$refs.rightSidenav.toggle();
+    },
+    closeRightSidenav() {
+      this.$refs.rightSidenav.close();
+    },
+    open(ref) {
+      console.log('Opened: ' + ref);
+    },
+    close(ref) {
+      console.log('Closed: ' + ref);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.whata-logo {
+  background: url('../assets/logo_whata.svg') top left;
+  background-size: cover;
+  display: inline-block;
+}
+
+.whata-logo-small {
+  width: 50px;
+  height: 55px;
+}
+</style>
---
+++ ui/src/components/WaterMap.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="water-map">
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+var L = require('leaflet')
+L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/'
+
+export default {
+  name: 'WaterMap',
+
+  mounted () {
+    this.map = L.map('map')
+    this.map.setView([48.7611542, 9.1943309], 13)
+    this.map.scrollWheelZoom.disable()
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map)
+  },
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "~leaflet/dist/leaflet.css";
+#map {
+  height: 600px;
+  z-index: 5;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
